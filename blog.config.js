bf7315bc8ec619309bcecb8653a232538c950e2e
Maqic <maqic51@gmail.com>
2023-11-20 14:59:39 +0800

    feat: update blog title

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'MTORY',
+  title: 'Maqic\'s Blog',
   author: 'Maqic Xu',
   email: 'maqic51@gmail.com',
   link: 'https://blog.maqicxu.com',
-  description: 'Maqic Story',
+  description: 'Maqic\'s Blog',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
